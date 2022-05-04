b9d3abeb6545ec2884f1449883afd1f576cada1c
Sveb <svebs@protonmail.com>
2022-05-04 10:47:15 +0200

    Home page posts

--- src/handlers/home.rs
+++ src/handlers/home.rs
@@ -1,9 +1,21 @@
 use super::user::{get_user_boards, UserBoards};
 use crate::handlers::user::{get_by_id, User};
 use actix_identity::Identity;
-use actix_web::{web, HttpResponse};
+use actix_web::{    web::{self, Query}, HttpResponse};
 use sailfish::TemplateOnce;
-use sqlx::MySqlPool;
+use sqlx::{FromRow, MySqlPool};
+use serde::{Deserialize,Serialize};
+
+
+#[derive(Serialize, FromRow, Debug)]
+pub struct BoardPost {
+    pub id: u32,
+    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub poster_id: u32,
+    pub board_name: Option<String>,
+    pub title: String,
+    pub text: String,
+}
 
 #[derive(TemplateOnce)]
 #[template(path = "home.stpl", escape = false)]
@@ -11,12 +23,18 @@ struct HomeTemplate {
     user: anyhow::Result<User>,
     top_boards: Vec<BoardEntry>,
     user_boards: Vec<UserBoards>,
+    posts: Vec<BoardPost>
 }
 
 struct BoardEntry {
     name: String,
     member_count: u64,
     icon: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct GetPostsQuery {
+    page: Option<u32>,
 }
 
 async fn get_top(pool: &MySqlPool) -> Result<Vec<BoardEntry>, sqlx::Error> {
@@ -34,7 +52,31 @@ limit 10;"#
     .await
 }
 
-pub async fn home(id: Identity, pool: web::Data<MySqlPool>) -> HttpResponse {
+pub async fn get_member_posts(
+    id: &u32,
+    pool: &MySqlPool,
+    limit: u32,
+    offset: u32,
+
+) -> Vec<BoardPost> {
+    sqlx::query_as!(
+        BoardPost,
+        r#"
+SELECT posts.id ,posts.created_at ,posts.poster_id ,( select name from boards where id = posts.board_id) as board_name ,posts.title ,posts.`text` FROM posts
+INNER JOIN members on members.board_id = posts.board_id 
+where members.user_id  = ?
+ORDER BY posts.created_at DESC
+limit ?
+offset ?;
+"#, id, limit, offset * limit
+    )
+    .fetch_all(pool)
+    .await
+    .unwrap_or_default()
+}
+
+
+pub async fn home(id: Identity, pool: web::Data<MySqlPool>, q: Query<GetPostsQuery>) -> HttpResponse {
     let id_int: u32 = id
         .identity()
         .unwrap_or_default()
@@ -46,6 +88,7 @@ pub async fn home(id: Identity, pool: web::Data<MySqlPool>) -> HttpResponse {
         user: user_res,
         top_boards: top,
         user_boards: get_user_boards(id_int, pool.get_ref()).await,
+        posts: get_member_posts(&id_int, pool.get_ref(), 10, q.page.unwrap_or_default()).await
     };
     HttpResponse::Ok().body(temp.render_once().unwrap())
 }
---
+++ static/home.js
@@ -0,0 +1,31 @@
+const posts = document.getElementById("posts");
+const posts_loading = posts.querySelector(".loading");
+const main = document.getElementById("content");
+const query = window.location.search;
+const params = new URLSearchParams(query);
+const baseurl = window.location.pathname;
+var offset = params.has("page") ? parseInt(params.get("page")) : 0;
+let parser = new DOMParser();
+
+const get_posts = async () => {
+  if (main.offsetHeight + main.scrollTop >= main.scrollHeight) {
+    posts_loading.style.display = "flex";
+await new Promise(r => setTimeout(r, 2000));
+    const url = baseurl + "?page=" + (offset + 1);
+    console.debug("Fetching at: " + url);
+    const res = await fetch(url);
+    const html = await res.text();
+    let html_parsed = parser.parseFromString(html, "text/html");
+    const page = html_parsed.querySelector(".page");
+    if (!page.childElementCount) {
+	// alert("no more posts")
+      posts_loading.style.display = "none";
+      await new Promise((r) => setTimeout(r, 2000));
+      return;
+    }
+    offset += 1;
+    // history.pushState({ page: offset }, "page " + offset, "?page=" + offset);
+    posts.insertBefore(page, posts_loading);
+  }
+};
+main.addEventListener("scroll", get_posts);
